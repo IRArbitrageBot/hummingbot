e5e9f82d8c2133d658e4b3f4bc41e0e6c7c06620
mhmb <m.h.motie@gmail.com>
2024-02-17 10:48:48 +0330

    feat: add authentication class and rest auth method for nobitex connector

---
+++ hummingbot/connector/exchange/nobitex/nobitex_auth.py
@@ -0,0 +1,34 @@
+import hashlib
+import hmac
+import json
+from collections import OrderedDict
+from typing import Any, Dict
+from urllib.parse import urlencode
+
+from hummingbot.connector.time_synchronizer import TimeSynchronizer
+from hummingbot.core.web_assistant.auth import AuthBase
+from hummingbot.core.web_assistant.connections.data_types import RESTMethod, RESTRequest, WSRequest
+
+class NobitexAuth(AuthBase):
+    def __init__(self, api_key: str, time_provider: TimeSynchronizer):
+        self.api_key = api_key
+        self.time_provider = time_provider
+
+    async def rest_authenticate(self, request: RESTRequest) -> RESTRequest:
+        """
+        Adds the server time and the signature to the request, required for authenticated interactions. It also adds
+        the required parameter in the request header.
+        :param request: the request to be configured for authenticated interaction
+        """
+        if request.method == RESTMethod.POST:
+            request.data = self.add_auth_to_params(params=json.loads(request.data) if request.data is not None else {})
+        else:
+            request.params = self.add_auth_to_params(params=request.params)
+
+        headers = {}
+        if request.headers is not None:
+            headers.update(request.headers)
+        headers.update(self.header_for_authentication())
+        request.headers = headers
+
+        return request
